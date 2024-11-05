d2122dbd741badff1ee23aaa820b81a0b84f2199
RexusPYM <praushkov.d@gmail.com>
2024-11-05 17:30:33 +0300

    made models

--- exchange/models.py
+++ exchange/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Seller(models.Model):
+    name = models.CharField(max_length=150)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Material(models.Model):
+    title = models.CharField(max_length=150)
+    seller = models.ForeignKey(Seller, on_delete=models.DO_NOTHING)
+    price_per_unit = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.title}"
